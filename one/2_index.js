cf92809eebb54a6f9d54fdbe9d1746ed939c27b7
Emir Puska <emir.puska@it-akademija.com>
2025-04-17 18:38:05 +0200

    Init push

---
+++ one/2_index.js
@@ -0,0 +1,10 @@
+function onSummerButtonClick() {
+    const imageElement = document.getElementById('seasonImage');
+    imageElement.src = 'summer.jpg';
+}
+
+
+function onWinterButtonClick() {
+    const imageElement = document.getElementById('seasonImage');
+    imageElement.src = 'winter.jpg';
+}
